752d50d17b5f5a20ac2704a36f50e12786e768e5
eduardvives74 <dev.eduardvives@gmail.com>
2020-08-03 16:40:48 -0500

    Subida a produccion :D

---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <hash-loader :loading="isLoading" :color="'#68d391'" :size="100" />
+    <px-assets-table v-if="!isLoading" :assets="assets" />
+  </div>
+</template>
+
+<script>
+import api from '@/api'
+import PxAssetsTable from '@/components/PxAssetsTable'
+import { HashLoader } from '@saeris/vue-spinners'
+export default {
+  name: 'Home',
+  components: { PxAssetsTable, HashLoader },
+  data() {
+    return {
+      isLoading: true,
+      assets: []
+    }
+  },
+
+  created() {
+    this.isLÑoading = true
+
+    api
+      .getAssets()
+      .then(assets => (this.assets = assets))
+      .finally(() => (this.isLoading = false))
+  }
+}
+</script>
